1bf483460d537fa95ffa7bac4252d41b284b8cc0
Mona Lisa <naomiroznik@gmail.com>
2022-10-07 13:42:44 -0600

    Defining variables in script.js and writing fetch call for current weather

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // api key 37ca395d783a15ec329da91c4fd43c95
-// 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityCode + "&limit=5&appid=37ca395d783a15ec329da91c4fd43c95"
+// 'http://api.openweathermap.org/geo/1.0/direct?q='+inputValue.value+'&limit=5&appid=37ca395d783a15ec329da91c4fd43c95'
 
 var button = document.querySelector('.button')
 var inputValue = document.querySelector('.inputValue')
@@ -7,3 +7,9 @@ var name = document.querySelector('.name')
 var temp = document.querySelector('.temp')
 var humidity = document.querySelector('.humidity')
 var wind = document.querySelector('.wind')
+
+fetch('http://api.openweathermap.org/geo/1.0/direct?q='+inputValue.value+'&limit=5&appid=37ca395d783a15ec329da91c4fd43c95')
+    .then(response => response.json())
+    .then(data => console.log(data))
+
+.catch(err => alert("Invalid entry"))
