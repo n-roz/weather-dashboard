50a0f29f702dee35a38e57817f41a46986fa60ac
Mona Lisa <naomiroznik@gmail.com>
2022-10-07 13:33:13 -0600

    Trying something different with the HTML so I can make the API call work

---
+++ script.js
@@ -0,0 +1,9 @@
+// api key 37ca395d783a15ec329da91c4fd43c95
+// 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityCode + "&limit=5&appid=37ca395d783a15ec329da91c4fd43c95"
+
+var button = document.querySelector('.button')
+var inputValue = document.querySelector('.inputValue')
+var name = document.querySelector('.name')
+var temp = document.querySelector('.temp')
+var humidity = document.querySelector('.humidity')
+var wind = document.querySelector('.wind')
