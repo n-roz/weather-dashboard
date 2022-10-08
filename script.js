5d907dad46b9137fd9098236de66c09024e607b6
Mona Lisa <naomiroznik@gmail.com>
2022-10-08 10:28:49 -0600

    Trying to get the JavaScript to display the current weather conditions but it is not working

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 // api key 37ca395d783a15ec329da91c4fd43c95
 // 'http://api.openweathermap.org/geo/1.0/direct?q='+inputValue.value+'&limit=5&appid=37ca395d783a15ec329da91c4fd43c95'
 
-var button = document.querySelector('.button')
-var inputValue = document.querySelector('.inputValue')
-var name = document.querySelector('.name')
+//var button = document.querySelector('.button')
+var input = document.querySelector('.input_text')
+var main = document.querySelector('.name')
 var temp = document.querySelector('.temp')
 var humidity = document.querySelector('.humidity')
 var wind = document.querySelector('.wind')
 
-fetch('http://api.openweathermap.org/geo/1.0/direct?q='+inputValue.value+'&limit=5&appid=37ca395d783a15ec329da91c4fd43c95')
+button.addEventListener('click',function(){
+fetch('http://api.openweathermap.org/geo/1.0/direct?q='+input.value+'&limit=5&appid=37ca395d783a15ec329da91c4fd43c95')
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => {
+        var tempValue = data['main']['temp'];
+        var nameValue = data['name'];
+    }
 
 .catch(err => alert("Invalid entry"))
+})
